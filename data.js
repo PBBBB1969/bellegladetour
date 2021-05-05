872a8351f06ad777753031e82ab7e8401d873987
PBBBB1969 <pbclstafftraining@gmail.com>
2021-05-05 10:20:40 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,413 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entry",
+      "name": "Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04458428882027654,
+          "pitch": 0.07474691133585765,
+          "rotation": 0,
+          "target": "1-past-mural-and-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-past-mural-and-entry",
+      "name": "Past Mural and Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05866977174837551,
+          "pitch": 0.33568076054400464,
+          "rotation": 0,
+          "target": "0-entry"
+        },
+        {
+          "yaw": -3.0205084602467505,
+          "pitch": 0.32658200616529953,
+          "rotation": 0,
+          "target": "7-reading-nook"
+        },
+        {
+          "yaw": 1.2431427921300422,
+          "pitch": 0.30869435564878955,
+          "rotation": 0,
+          "target": "2-main-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-main-area",
+      "name": "Main Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.272677191262762,
+          "pitch": 0.2867317015470441,
+          "rotation": 0,
+          "target": "8-teen-room"
+        },
+        {
+          "yaw": -0.7501501470214187,
+          "pitch": 0.25367885119948674,
+          "rotation": 0,
+          "target": "1-past-mural-and-entry"
+        },
+        {
+          "yaw": 0.808912505041194,
+          "pitch": 0.23107162623495192,
+          "rotation": 0,
+          "target": "3-future-mural"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-future-mural",
+      "name": "Future Mural",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.032383685665719,
+          "pitch": 0.3449364463837803,
+          "rotation": 0,
+          "target": "8-teen-room"
+        },
+        {
+          "yaw": -1.0595576217821598,
+          "pitch": 0.564885995100763,
+          "rotation": 0,
+          "target": "2-main-area"
+        },
+        {
+          "yaw": 0.9930220343001093,
+          "pitch": 0.5345149435500396,
+          "rotation": 0,
+          "target": "4-childrens-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-childrens-entry",
+      "name": "Children's Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.680568430644943,
+          "pitch": 0.2931302814793568,
+          "rotation": 0,
+          "target": "3-future-mural"
+        },
+        {
+          "yaw": 1.5114932986122822,
+          "pitch": 0.1479163638351899,
+          "rotation": 0,
+          "target": "5-childrens-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-childrens-area",
+      "name": "Children's Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9194167514675939,
+          "pitch": 0.23398129516554889,
+          "rotation": 0,
+          "target": "4-childrens-entry"
+        },
+        {
+          "yaw": 3.0777573698862035,
+          "pitch": 0.23681251184071428,
+          "rotation": 0,
+          "target": "6-storytime-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-storytime-room",
+      "name": "Storytime Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.44840000902219046,
+          "pitch": 0.26836151533298924,
+          "rotation": 0,
+          "target": "5-childrens-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-reading-nook",
+      "name": "Reading Nook",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6372047312550002,
+          "pitch": 0.33943782024018176,
+          "rotation": 0,
+          "target": "1-past-mural-and-entry"
+        },
+        {
+          "yaw": -0.6253053298578326,
+          "pitch": 0.3054685901243648,
+          "rotation": 0,
+          "target": "2-main-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-teen-room",
+      "name": "Teen Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1528,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.285224082589302,
+          "pitch": 0.26656403162673925,
+          "rotation": 0,
+          "target": "3-future-mural"
+        },
+        {
+          "yaw": -1.772444515207054,
+          "pitch": 0.28480030299259695,
+          "rotation": 0,
+          "target": "2-main-area"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Belle Glade Library",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
